ccdae2887eb2c01f8534459279c0ae8cf8c3379c
Daniel Rha <hyunjoe-rha@hotmail.com>
2021-03-06 14:02:21 -0500

    more fixes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function() {
     baguetteBox.run('.gallery')
    });
-   const search = new Filter('search','data-caption');
+   const search = new Filter('search-input','data-caption');
